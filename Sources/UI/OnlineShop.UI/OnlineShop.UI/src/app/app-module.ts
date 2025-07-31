6b164d2193eb490573331ca9734722ab19cf2f1d
Y.Kharitonchik@enova.by <Y.Kharitonchik@enova.by>
2025-07-31 12:14:20 +0300

    Added category-list

--- Sources/UI/OnlineShop.UI/OnlineShop.UI/src/app/app-module.ts
+++ Sources/UI/OnlineShop.UI/OnlineShop.UI/src/app/app-module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Navbar } from './Core/Components/navbar/navbar';
+import { CategoryList } from './Core/Features/Category/category-list/category-list';
 
 @NgModule({
   declarations: [
     App,
-    Navbar
+    Navbar,
+    CategoryList
   ],
   imports: [
     BrowserModule,
--- Sources/UI/OnlineShop.UI/OnlineShop.UI/src/app/app-routing-module.ts
+++ Sources/UI/OnlineShop.UI/OnlineShop.UI/src/app/app-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryList } from './Core/Features/Category/category-list/category-list';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "admin/categories",
+    component: CategoryList
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
